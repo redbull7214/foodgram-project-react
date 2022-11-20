4ccb8906ab587a601a64f39daf997e1f4a11bae1
Никита Малин <redbull7214@gmail.com>
2022-11-20 21:46:14 +0300

    test

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -157,7 +157,7 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.TokenAuthentication',
     ),
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 6
+    'PAGE_SIZE': 2
 }
 
 DJOSER = {
