ab4dd6841c105bbfcf0c7f466bd09ac79cef7ecd
Никита Малин <redbull7214@gmail.com>
2022-11-26 12:55:59 +0300

    for review

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -29,6 +29,7 @@ urlpatterns += [
             schema_view.without_ui(cache_timeout=0), name='schema-json'),
     re_path(r'^swagger/$', schema_view.with_ui('swagger',
             cache_timeout=0), name='schema-swagger-ui'),
-    re_path(r'^redoc/$', schema_view.with_ui('redoc',
-                                             cache_timeout=0), name='schema-redoc'),
+    re_path(r'^redoc/$',
+            schema_view.with_ui('redoc',
+                                cache_timeout=0), name='schema-redoc'),
 ]
