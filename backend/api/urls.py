f0260686b50785b153850bd03434d08b8b2ce3e2
Никита Малин <redbull7214@gmail.com>
2022-11-26 13:02:21 +0300

    for review

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,9 +1,24 @@
-from django.urls import include, path
+from django.urls import include, path, re_path
 from rest_framework.routers import DefaultRouter
 
 from .views import (CustomUserViewSet, FollowListView, FollowViewSet,
                     IngredientsViewSet, RecipeViewSet, TagsViewSet)
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
 
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Snippets API",
+        default_version='v1',
+        description="Test description",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="contact@snippets.local"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=[permissions.AllowAny],
+)
 
 app_name = 'api'
 router_v1 = DefaultRouter()
@@ -27,4 +42,13 @@ urlpatterns = [
     path('', include(router_v1.urls)),
     path('', include('djoser.urls')),
     path('auth/', include('djoser.urls.authtoken')),
+]
+urlpatterns += [
+    re_path(r'^swagger(?P<format>\.json|\.yaml)$',
+            schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    re_path(r'^swagger/$', schema_view.with_ui('swagger',
+            cache_timeout=0), name='schema-swagger-ui'),
+    re_path(r'^redoc/$',
+            schema_view.with_ui('redoc',
+                                cache_timeout=0), name='schema-redoc'),
 ]
